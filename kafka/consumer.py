97e00a0b06ec86beb8fa37fc7f267ee76f667a21
Vasudha Tulla <tullavasudha@gmail.com>
2026-01-25 12:03:40 +0530

    changes done

--- kafka/consumer.py
+++ kafka/consumer.py
@@ -5,34 +5,42 @@ import os
 
 consumer = KafkaConsumer(
     'price-data',
-    bootstrap_servers = 'localhost:9092',
-    auto_offset_reset = 'earliest',
-    value_deserializer = lambda x:json.loads(x.decode('utf-8'))
+    bootstrap_servers='localhost:9092',
+    auto_offset_reset='earliest',
+    enable_auto_commit=True,
+    group_id='price_consumer_v1',
+    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
 )
 
-os.makedirs('../data',exist_ok = True)
+os.makedirs('../data', exist_ok=True)
 filepath = '../data/streaming_data.csv'
 
 file_exists = os.path.isfile(filepath)
 
-with open(filepath,mode = 'a',newline = "") as file:
-    writer = csv.writer(file)
+print("Kafka Consumer started... Saving data to CSV")
+
+file = open(filepath, mode='a', newline="")
+writer = csv.writer(file)
 
 if not file_exists:
-    writer.writerow(['product_id','price','demand','timestamp'])
+    writer.writerow(['product_id', 'price', 'timestamp'])
+    file.flush()
 
-print(" Saving Kafka data to CSV...")
+try:
+    for message in consumer:
+        data = message.value
 
-for message in consumer:
-    data = message.value
-
-writer.writerow([
+        writer.writerow([
             data['product_id'],
             data['price'],
-            data['demand'],
             data['timestamp']
         ])
+        file.flush()
 
-        
-print("Saved:", data)
+        print("Saved:", data)
 
+except KeyboardInterrupt:
+    print("Stopping consumer...")
+
+finally:
+    file.close()
