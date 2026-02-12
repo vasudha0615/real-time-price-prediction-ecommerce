f39a4e309286407d4a5cc02d1a39afdbeebf9cae
Vasudha Tulla <tullavasudha@gmail.com>
2026-02-12 18:01:44 +0530

    add kafka producer

--- kafka/producer.py
+++ kafka/producer.py
@@ -2,27 +2,27 @@ from kafka import KafkaProducer
 import json
 import time
 import random
+from datetime import datetime, timedelta
 
-# Create Kafka producer
 producer = KafkaProducer(
     bootstrap_servers='localhost:9092',
-    value_serializer=lambda v: json.dumps(v).encode('utf-8')
+    value_serializer=lambda v: json.dumps(v, default=str).encode('utf-8')
 )
 
 topic_name = "price-data"
-
 print("Kafka Producer started...")
 
 while True:
+    past_days = random.randint(1, 90)
+    event_time = datetime.now() - timedelta(days=past_days)
+
     data = {
-        "product_id": random.randint(1000, 1010),
-        "price": round(random.uniform(200, 1500), 2),
-        "timestamp": time.time()
+        "quantity": round(random.uniform(1, 10), 2),
+        "customer_id":random.randint(1000, 1010),
+        "predicted_at": event_time
     }
 
     producer.send(topic_name, value=data)
     print("Sent:", data)
 
     time.sleep(5)
-
-
